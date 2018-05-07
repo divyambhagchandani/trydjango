ea43e47669dd63772d64ce38bc7cb3c8b82d7192
divyambhagchandani <divyambhagchandani@gmail.com>
2018-05-08 02:57:32 +0530

    Start

---
+++ lib/python3.6/stat.py
@@ -0,0 +1,1 @@
+/Users/divyambhagchandani/anaconda/lib/python3.6/stat.py
---
+++ src/restaurants/views.py
@@ -0,0 +1,33 @@
+from django.http import HttpResponse
+from django.shortcuts import render,get_object_or_404
+from django.views import View
+from django.views.generic import TemplateView, ListView, DetailView
+
+from .models import RestaurantLocation
+# Create your views here.
+
+def restaurant_listview(request):
+	template_name= 'restaurants/restaurants_list.html'
+	Queryset=RestaurantLocation.objects.all()
+	context={
+	"object_list":Queryset
+	}
+	return render(request, template_name, context)
+class RestaurantListView(ListView):
+	def get_queryset(self):
+		slug = self.kwargs.get("slug")
+		if slug:
+			queryset=RestaurantLocation.objects.filter(category__iexact=slug)
+		else:
+			queryset=RestaurantLocation.objects.all()
+		return queryset
+class RestaurantDetailView(DetailView):
+			queryset=RestaurantLocation.objects.all()
+			# def get_context_data(self,*args, **kwargs):
+			# 	print(self.kwargs)
+			# 	context=super(RestaurantDetailView, self).get_context_data(*args,**kwargs)
+			# 	print(context)
+			# def get_object(self,*args,**kwargs):
+			# 	rest_id= self.kwargs.get('rest_id')
+			# 	obj=get_object_or_404(RestaurantLocation, id=rest_id) #pk=rest_id
+			# 	return obj
